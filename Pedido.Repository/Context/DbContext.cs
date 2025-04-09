a0966119e8cc4c835eb4bf2ba1e8ebf913a21c66
Guilherme <guilhermemcl1503@gmail.com>
2025-04-08 21:07:18 -0300

    Feat : Criando conexao com o banco de dados

---
+++ Pedido.Repository/Context/DbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Pedido.Repository.Context
+{
+    public class DbContext
+    {
+
+        private readonly string _conn;
+        public DbContext()
+        {
+            _conn = "Server=localhost;Port=3306;Database=pedidosdb;Uid=root;Pwd=root;" ;
+        }
+
+        public MySqlConnection ObterConexao()
+        {
+            var conexao = new MySqlConnection(_conn);
+            conexao.Open();
+            return conexao;
+        }
+
+
+    }
+}
